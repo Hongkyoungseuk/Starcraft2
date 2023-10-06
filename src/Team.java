5f80854fab7cb3cdf726730edf6baf4be0fb03f8
{Hongkyoungseuk} <tug234@daum.net>
2023-10-06 10:06:21 +0900

    10.06 10:06

--- src/Team.java
+++ src/Team.java
@@ -1,10 +1,38 @@
 import java.util.ArrayList;
 
 public class Team {
-    ArrayList<Enum> units = new ArrayList<>();
+    private ArrayList<Enum> units = new ArrayList<>();
+    private int teamSize;
 
-    Team() {
-        int random = (int)(Terran.values().length * Math.random());
-        units.add(Terran.values()[random]);
+    Team(char t) {
+        if (t == 'T' || t == 't') { // Terran
+            this.teamSize = 5;
+            int random = (int)(Terran.values().length * Math.random());
+            units.add(Terran.values()[random]);
+        } else if (t == 'Z' || t == 'z') {  // Zerg
+            this.teamSize = 8;
+            int random = (int)(Zerg.values().length * Math.random());
+            units.add(Zerg.values()[random]);
+        } else if (t == 'P' || t == 'p') {  // Protos
+            this.teamSize = 4;
+            int random = (int)(Protos.values().length * Math.random());
+            units.add(Protos.values()[random]);
+        } else {
+            this.teamSize = 0;
+            System.out.println("팀이 만들어지지 않았습니다.");
+        }
+    }
+
+
+    public ArrayList<Enum> getUnits() {
+        return units;
+    }
+
+    public Enum getUnit(int i) {
+        return units.get(i);
+    }
+
+    public int getTeamSize() {
+        return teamSize;
     }
 }
